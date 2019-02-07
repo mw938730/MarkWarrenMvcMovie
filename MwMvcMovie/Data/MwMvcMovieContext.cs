97fc19a906d7516303765063d895d2763e6f3bb8
Mark <mcwarren1@buffs.wtamu.edu>
2019-02-06 23:55:46 -0600

    Added MVCMovie
    Add init commit

---
+++ MwMvcMovie/Data/MwMvcMovieContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MwMvcMovie.Models
+{
+    public class MwMvcMovieContext : DbContext
+    {
+        public MwMvcMovieContext (DbContextOptions<MwMvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; }
+    }
+}
